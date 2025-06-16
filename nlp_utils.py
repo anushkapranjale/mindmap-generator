edc566119b2d8fc894b635efaf3fbd827bfdfbea
Anushka Pranjale <anushkapranjale19@gmail.com>
2025-06-16 20:39:17 +0530

    Fix: Added spaCy model auto-download

--- nlp_utils.py
+++ nlp_utils.py
@@ -1,16 +1,17 @@
 import spacy
+import subprocess
 
-nlp = spacy.load("en_core_web_sm")
-
-# 👇 Optional: apni list of boring/common words jo ignore karne hain
-BLOCKLIST = { "type", "thing", "way", "data", "information"}
+# Download model if not present
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
 
 def extract_keywords(text):
     doc = nlp(text)
-    keywords = [
-        token.lemma_ for token in doc
-        if token.pos_ in ['NOUN', 'PROPN']
-        and not token.is_stop
-        and token.lemma_.lower() not in BLOCKLIST
-    ]
-    return list(set(keywords))[:10]  # 👈 unique and top 10
+    keywords = set()
+    for token in doc:
+        if token.pos_ in ["NOUN", "PROPN"]:
+            keywords.add(token.text)
+    return list(keywords)
